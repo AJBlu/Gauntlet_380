4f4ae06f3c3d0fb5801195fed75d510f450a7462
BryanARamirez <103203508+BryanARamirez@users.noreply.github.com>
2024-04-16 13:45:21 -0700

    Camera Clamp Working
    Just need to make it get the central point of players.

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -9,24 +9,28 @@ public class CameraController : MonoBehaviour
     private float cameraY;
     private float minPositionX;
     private float maxPositionX;
+    private float minPositionZ;
+    private float maxPositionZ;
 
     private void Awake()
     {
         cameraY = transform.position.y;
-        minPositionX = -10f;
-        minPositionX = 10f;
+        //This needs to be changed to whatever the levels size is
+        minPositionX = -4f;
+        maxPositionX = 28f;
+        minPositionZ = -7.5f;
+        maxPositionZ = 32.5f;
     }
 
     private void Update()
     {
-        transform.position = new Vector3(Mathf.Clamp(transform.position.x, minPositionX, maxPositionX), transform.position.y, transform.position.z);
         farthestPlayer = GameObject.FindGameObjectWithTag("Player").transform.position;
         farthestPlayer.y = cameraY;
-        if(farthestPlayer != null)
+        if (farthestPlayer != null)
         {
             if (transform.position.x != farthestPlayer.x || transform.position.z != farthestPlayer.z)
             {
-                transform.position = new Vector3(farthestPlayer.x, cameraY, farthestPlayer.z);
+                transform.position = new Vector3(Mathf.Clamp(farthestPlayer.x, minPositionX, maxPositionX), cameraY, Mathf.Clamp(farthestPlayer.z, minPositionX, maxPositionX));
             }
         }
     }
