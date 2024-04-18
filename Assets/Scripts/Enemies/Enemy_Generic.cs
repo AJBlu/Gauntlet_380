3a07ebebe1c4dda89ada15e1add9ec13c85e78a0
AJBlu <70176737+AJBlu@users.noreply.github.com>
2024-04-18 10:34:03 -0700

    generic

---
+++ Assets/Scripts/Enemies/Enemy_Generic.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements.Experimental;
+
+public class Enemy_Generic : IEnemy, IDamageable
+{
+    //Damageable
+   public void assignDamageStats()
+   {
+
+   }
+
+   public void onDamage()
+   {
+
+   }
+
+   public void onDeath()
+   {
+
+   }
+
+    //Enemy
+    public void assignScoringValues()
+    {
+
+    }
+
+    public void Attack()
+    {
+
+    }
+}
