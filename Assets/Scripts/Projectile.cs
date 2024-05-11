80cf9148af8c1383189d9e19ffd13436ae1d467a
AJBlu <70176737+AJBlu@users.noreply.github.com>
2024-05-10 20:10:57 -0700

    magic_ready

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    int _damage;
+    float _shotSpeed;
+    Vector3 _movementDirection;
+    GameObject _origin;
+    public void SetShotAttributes(int damage, float shotSpeed, Vector3 movementDirection, GameObject origin)
+    {
+        damage = _damage;
+        shotSpeed = _shotSpeed;
+        _movementDirection = movementDirection;
+        _origin = origin;
+
+    }
+    private void Update()
+    {
+        _movement();
+
+    }
+
+    private void _movement()
+    {
+
+    }
+}
