b1f1926f14723409938df91ea37f7db9511934a7
AJBlu <70176737+AJBlu@users.noreply.github.com>
2024-05-14 10:41:58 -0700

    ughhh

--- Assets/Scripts/Enemies/Fighters/Ghost.cs
+++ Assets/Scripts/Enemies/Fighters/Ghost.cs
@@ -7,6 +7,6 @@ public class Ghost : Fighter_Generic
     public override void attack(GameObject player)
     {
         base.attack(player);
-        Destroy(this);
+        Destroy(gameObject);
     }
 }
